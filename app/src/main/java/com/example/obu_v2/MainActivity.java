7ee0717a65f855eacc8fd777b687246399976fab
lin <haha80086@gmail.com>
2020-01-12 19:33:02 +0800

    new layout

--- app/src/main/java/com/example/obu_v2/MainActivity.java
+++ app/src/main/java/com/example/obu_v2/MainActivity.java
@@ -3,15 +3,11 @@ package com.example.obu_v2;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.ClipData;
-import android.content.ClipboardManager;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -24,12 +20,9 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.Settings;
 import android.text.method.ScrollingMovementMethod;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -38,19 +31,17 @@ import android.widget.Toast;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.nio.file.Files;
+import java.sql.Time;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
@@ -73,33 +64,40 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private Sensor acc;
     private Sensor gyro;
 
-    private String filePath_gps;
-    private String filePath_acc;
-    private String filePath_gyro;
-
     private File output_gps;
     private File output_acc;
     private File output_gyro;
 
-    private Calendar rightNow;
+    private Timer mTimer;
+
+    private int timer_period;
+
+    // updating container
+    private double lat, lng, speed;
+    private double acc_x, acc_y, acc_z;
+    private double gyro_x, gyro_y, gyro_z;
+
     private static final int REQUEST_EXTERNAL_STORAGE = 1;
-    private static String[] PERMISSIONS_STORAGE = {
+    private static String[] PERMISSIONS = {
             "android.permission.READ_EXTERNAL_STORAGE",
-            "android.permission.WRITE_EXTERNAL_STORAGE" };
+            "android.permission.WRITE_EXTERNAL_STORAGE",
+            "android.permission.ACCESS_COARSE_LOCATION",
+            "android.permission.ACCESS_FINE_LOCATION"};
 
-
-    public static void verifyStoragePermissions(Activity activity) {
-
+    public static void verifyALLPermissions(Activity activity) {
+        int PERMISSIONS_ALL = 1;
         try {
-            int permission = ActivityCompat.checkSelfPermission(activity,
-                    "android.permission.WRITE_EXTERNAL_STORAGE");
-            if (permission != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(activity, PERMISSIONS_STORAGE,REQUEST_EXTERNAL_STORAGE);
+
+            for(String permission: PERMISSIONS){
+                if(ActivityCompat.checkSelfPermission(activity, permission) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(activity, PERMISSIONS, PERMISSIONS_ALL);
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -120,28 +118,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         history_gyro_data.setMovementMethod(new ScrollingMovementMethod());
 
 
-        // get GPS permission
-        if(!lc.isProviderEnabled(LocationManager.GPS_PROVIDER)){
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setTitle("定位管理")
-                    .setMessage("GPS not enable.\n" + "please enable")
-                    .setPositiveButton("啟用",
-                            new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    Intent i = new Intent(
-                                            Settings.ACTION_LOCALE_SETTINGS);
-                                    startActivity(i);
-                                }
-                            })
-                    .setNegativeButton("不啟用", null).create().show();
-        }
-        if(checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
-        }
-
-
-        verifyStoragePermissions( this );
+        verifyALLPermissions( this );
 
         // dir & file init
         String root = getExternalFilesDir(null).toString();
@@ -149,14 +126,19 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         if(!dir.mkdirs() && !dir.exists()){
             Toast.makeText(MainActivity.this, "Failed to make dir", Toast.LENGTH_SHORT).show();
         }
-        filePath_gps = dir + File.separator + "gps.txt";
-        filePath_acc = dir + File.separator + "acc.txt";
-        filePath_gyro = dir + File.separator + "gyro.txt";
+        String filePath_gps = dir + File.separator + "gps.txt";
+        String filePath_acc = dir + File.separator + "acc.txt";
+        String filePath_gyro = dir + File.separator + "gyro.txt";
 
         try{
             output_acc = new File(filePath_acc);
             output_gps = new File(filePath_gps);
             output_gyro = new File(filePath_gyro);
+            if(output_acc.exists() || output_gyro.exists() || output_gps.exists()){
+                output_gps.delete();
+                output_acc.delete();
+                output_gyro.delete();
+            }
             output_acc.createNewFile();
             output_gps.createNewFile();
             output_gyro.createNewFile();
@@ -180,7 +162,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 if(temp.equals("Start")){
                     // register sensor
                     sm = (SensorManager) getSystemService(SENSOR_SERVICE);
-                    acc = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+                    acc = sm.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
                     gyro = sm.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
                     sm.registerListener(MainActivity.this, acc, SensorManager.SENSOR_DELAY_NORMAL);
                     sm.registerListener(MainActivity.this, gyro, SensorManager.SENSOR_DELAY_NORMAL);
@@ -196,6 +178,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                     }
                     button_copy.setEnabled(false);
                     button_start.setText("Stop");
+
+                    mTimer = new Timer();
+                    setTimerTask();
                 }
                 else if(temp.equals("Stop")){
                     // unregister sensor
@@ -209,15 +194,48 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
                     button_copy.setEnabled(true);
                     button_start.setText("Start");
+                    mTimer.cancel();
                 }
                 else{
                     button_start.setText("Error");
                 }
             }
         });
-
-
     }
+
+    private void setTimerTask() {
+        mTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                // updating gps info
+                long roundtime = System.currentTimeMillis();
+                String temp = System.currentTimeMillis() + " " + lat + " " + lng + " " + speed + "\n";
+                current_gps.setText("Current information: \n時間: " + roundtime +
+                        "\n經度: "+ lng + "\n緯度: " + lat + "\n速度: " + speed);
+                history_gps_data.append(temp);
+                writeToFile(temp, "gps");
+
+                String values = "X-axis: " + acc_x + "\n" +
+                                    "Y-axis: " + acc_y + "\n" +
+                                    "Z-axis: " + acc_z;
+                // String date = new SimpleDateFormat("HH:mm:ss:SSS").format(new Date());
+                temp = roundtime + " " + acc_x + " " + acc_y + " " + acc_z + "\n";
+                //updating acc info
+                accelerometers.setText("Accelerometers\n" + values);
+                history_acc_data.append(temp);
+                writeToFile(temp, "acc");
+
+                values = "X-axis: " + gyro_x + "\n" +
+                        "Y-axis: " + gyro_y + "\n" +
+                        "Z-axis: " + gyro_z;
+                temp = roundtime + " " + gyro_x + " " + gyro_y + " " + gyro_z + "\n";
+                gyroscopes.setText("Gyroscopes\n" + values);
+                history_gyro_data.append(temp);
+                writeToFile(temp, "gyro");
+            }
+        }, 1000, 1000);
+    }
+
     @Override
     // After get the permissions
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResult){
@@ -240,44 +258,32 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         output_gps.delete();
         output_acc.delete();
         output_gyro.delete();
+        mTimer.cancel();
     }
     @Override
     // sensor function
     public void onSensorChanged(SensorEvent event){
-        String values = "X-axis: " + event.values[0] + "\n" +
-                "Y-axis: " + event.values[1] + "\n" +
-                "Z-axis: " + event.values[2];
-        rightNow = Calendar.getInstance();
-        String temp = rightNow.getTimeInMillis()+ " " + event.values[0] + " " + event.values[1] + " " + event.values[2] + "\n";
         if(event.sensor.equals(acc)){
-            accelerometers.setText("Accelerometers\n" + values);
-            history_acc_data.append(temp);
-            writeToFile(temp, "acc");
+            acc_x = event.values[0];
+            acc_y = event.values[1];
+            acc_z = event.values[2];
         }
         if(event.sensor.equals(gyro)){
-            gyroscopes.setText("Gyroscopes\n" + values);
-            history_gyro_data.append(temp);
-            writeToFile(temp, "gyro");
+            gyro_x = event.values[0];
+            gyro_y = event.values[1];
+            gyro_z = event.values[2];
         }
     }
-    public void onAccuracyChanged(Sensor sensor, int accuracy){ } //unchanged
+    public void onAccuracyChanged(Sensor sensor, int accuracy){ }
 
     private class MyLocationListener implements LocationListener {
         @SuppressLint("SetTextI18n")
         public void onLocationChanged(Location current){
-            double lat, lng, speed;
-            rightNow = Calendar.getInstance();
+            // String date = new SimpleDateFormat("HH:mm:ss:SSS").format(new Date());
             if(current != null){
-                lat = current.getLatitude();
                 lng = current.getLongitude();
+                lat = current.getLatitude();
                 speed = current.getSpeed() * 3.6;
-                String temp = current.getTime() + " " + lat + " " + lng + " " + speed + "\n";
-                // time format: Return the UTC time of this fix, in milliseconds since January 1, 1970.
-                current_gps.setText("Current information: \n時間: " + rightNow.getTimeInMillis() +
-                        "\n經度: "+ lat + "\n緯度: " + lng + "\n速度: " + speed);
-                history_gps_data.append(temp);
-                // write file
-                writeToFile(temp, "gps");
             }
         }
         public void onProviderDisabled(String provider){}
@@ -344,7 +350,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         Intent i = new Intent(Intent.ACTION_SEND_MULTIPLE);
         i.setType("vnd.android.cursor.dir/email");
         i.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-        i.putExtra(Intent.EXTRA_EMAIL, new String[]{"ne6080156@gs.ncku.edu.tw"});
+        i.putExtra(Intent.EXTRA_EMAIL, new String[]{"ne6081056@gs.ncku.edu.tw"});
         i.putExtra(Intent.EXTRA_SUBJECT, "Experiment outcome");
         i.putParcelableArrayListExtra(Intent.EXTRA_STREAM, uris);
         try{
@@ -354,3 +360,29 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         }
     }
 }
+
+
+// useless code
+
+
+// get GPS permission
+//        if(!lc.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+//            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//            builder.setTitle("定位管理")
+//                    .setMessage("GPS not enable.\n" + "please enable")
+//                    .setPositiveButton("啟用",
+//                            new DialogInterface.OnClickListener() {
+//                                @Override
+//                                public void onClick(DialogInterface dialog, int which) {
+//                                    Intent i = new Intent(
+//                                            Settings.ACTION_LOCALE_SETTINGS);
+//                                    startActivity(i);
+//                                }
+//                            })
+//                    .setNegativeButton("不啟用", null).create().show();
+//        }
+//        if(checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+//            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+//        }
+//
+
